d01cdb70c9b196043876291a9295f722cf4da922
DayronIntriago567 <juesusintriago56@gmail.com>
2026-08-18 23:22:01 -0500

    Corrige mapeo de Cliente/Bicicleta en vista de Ventas y agrega links de navegacion

--- BikeStore.Web/Models/DetalleVenta.cs
+++ BikeStore.Web/Models/DetalleVenta.cs
@@ -2,12 +2,10 @@ namespace BikeStore.Web.Models
 {
     public class DetalleVenta
     {
-        public int IdDetalle { get; set; }
-        public int IdVenta { get; set; }
         public int IdBicicleta { get; set; }
+        public string? BicicletaDescripcion { get; set; }
         public int Cantidad { get; set; }
         public decimal Precio { get; set; }
         public decimal Subtotal { get; set; }
-        public Bicicleta? Bicicleta { get; set; }
     }
 }
